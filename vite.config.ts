e7195284bc7ca1db0ca222a25b02b924e722b693
aduvermy <arnaud.duvermy@inria.fr>
2025-02-10 11:55:30 +0100

    fix: update base URL to match repository name case

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // GitHub Pages repository name
+  base: '/portfolio/', // GitHub Pages repository name
   build: {
     outDir: 'dist',
     sourcemap: true,
